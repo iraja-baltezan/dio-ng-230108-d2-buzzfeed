7e1d578465761fcb511b6a1d021666a3d3126cb0
iraja-baltezan <iraja.baltezan@gmail.com>
2023-01-12 00:39:19 -0300

    feat: presentation and basic functionality

--- ngbuzz/src/app/components/quizz/quizz.component.ts
+++ ngbuzz/src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import quizz_questions from 'src/assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -6,5 +7,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./quizz.component.scss']
 })
 export class QuizzComponent {
+
+  title: string = 'Título';
+
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+      this.questions = quizz_questions.questions;
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+      this.questionSelected = this.questions[this.questionIndex];
+
+    }
+
+  }
+
+  optionOnClick(alias: string): void {
+    this.answers.push(alias);
+    this.nextStep();
+  }
+
+  nextStep(): void {
+    this.questionIndex++;
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    }
+    else {
+      const finalAnswer: string = this.checkResult(this.answers);
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
+      this.finished = true;
+
+    }
+  }
+
+  checkResult(answers: string[]): string {
+    const result = answers.reduce(
+      (previous, current, i, arr) => {
+        if (
+          arr.filter(item => item === previous).length >
+          arr.filter(item => item === current).length
+        ) {
+          return previous;
+        }
+        else {
+          return current;
+        }
+      }
+    )
+    return result;
+  }
 
 }
